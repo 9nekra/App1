ca0d48b5d25e01787f000c3c368fb4e3a761f049
rhaegys <rhaegys@gmail.com>
2017-08-13 20:12:28 +0700

    backup

--- app/Http/Controllers/InputController.php
+++ app/Http/Controllers/InputController.php
@@ -5,11 +5,85 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use phpDocumentor\Reflection\Types\Nullable;
+require 'C:\Users\MagmaDiver\app\vendor/autoload.php'; //надо PATH поковырять
+use Elasticsearch\ClientBuilder;
 
+ini_set('max_execution_time', 36000);
 
 class InputController extends Controller
 {
     //
+//  функция для деления строки на слова по пробелам и тире
+    function multiexplode($delimiters, $string)
+    {
+
+        $ready = str_replace($delimiters, $delimiters[0], $string);
+        $launch = explode($delimiters[0], $ready);
+        return $launch;
+    }
+
+    // функция для восстановки нормальных ключей при выборочном заполнении массива
+    function setOrder($array)
+    {
+        $countX = 0;
+        $resultT = array();
+        foreach ($array as $temp1) {
+            $resultT[$countX] = rtrim($temp1);
+            $countX++;
+        }
+        return $resultT;
+
+    }
+
+    // функция для удаления повторов из 2d массива
+    function super_unique($array)
+    {
+        $result = array_map("unserialize", array_unique(array_map("serialize", $array)));
+
+        foreach ($result as $key => $value) {
+            if (is_array($value)) {
+                $result[$key] = super_unique($value);
+            }
+        }
+
+        return $result;
+    }
+    // функция для сортировки массива по ключу
+    function array_sort($array, $on, $order=SORT_ASC)
+    {
+        $new_array = array();
+        $sortable_array = array();
+
+        if (count($array) > 0) {
+            foreach ($array as $k => $v) {
+                if (is_array($v)) {
+                    foreach ($v as $k2 => $v2) {
+                        if ($k2 == $on) {
+                            $sortable_array[$k] = $v2;
+                        }
+                    }
+                } else {
+                    $sortable_array[$k] = $v;
+                }
+            }
+
+            switch ($order) {
+                case SORT_ASC:
+                    asort($sortable_array);
+                    break;
+                case SORT_DESC:
+                    arsort($sortable_array);
+                    break;
+            }
+
+            foreach ($sortable_array as $k => $v) {
+                $new_array[$k] = $array[$k];
+            }
+        }
+
+        return $new_array;
+    }
+
 
     public function index()
     {
@@ -19,79 +93,6 @@ class InputController extends Controller
 
     public function process()
     {
-        //  функция для деления строки на слова по пробелам и тире
-        function multiexplode($delimiters, $string)
-        {
-
-            $ready = str_replace($delimiters, $delimiters[0], $string);
-            $launch = explode($delimiters[0], $ready);
-            return $launch;
-        }
-
-        // функция для восстановки нормальных ключей при выборочном заполнении массива
-        function setOrder($array)
-        {
-            $countX = 0;
-            $resultT = array();
-            foreach ($array as $temp1) {
-                $resultT[$countX] = rtrim($temp1);
-                $countX++;
-            }
-            return $resultT;
-
-        }
-
-        // функция для удаления повторов из 2d массива
-        function super_unique($array)
-        {
-            $result = array_map("unserialize", array_unique(array_map("serialize", $array)));
-
-            foreach ($result as $key => $value) {
-                if (is_array($value)) {
-                    $result[$key] = super_unique($value);
-                }
-            }
-
-            return $result;
-        }
-        // функция для сортировки массива по ключу
-        function array_sort($array, $on, $order=SORT_ASC)
-        {
-            $new_array = array();
-            $sortable_array = array();
-
-            if (count($array) > 0) {
-                foreach ($array as $k => $v) {
-                    if (is_array($v)) {
-                        foreach ($v as $k2 => $v2) {
-                            if ($k2 == $on) {
-                                $sortable_array[$k] = $v2;
-                            }
-                        }
-                    } else {
-                        $sortable_array[$k] = $v;
-                    }
-                }
-
-                switch ($order) {
-                    case SORT_ASC:
-                        asort($sortable_array);
-                        break;
-                    case SORT_DESC:
-                        arsort($sortable_array);
-                        break;
-                }
-
-                foreach ($sortable_array as $k => $v) {
-                    $new_array[$k] = $array[$k];
-                }
-            }
-
-            return $new_array;
-        }
-
-
-
         //ввожу данные
         $input = $_POST["input"];
         $inputstr = explode("\n", $input);
@@ -117,19 +118,19 @@ class InputController extends Controller
 
         //вывод полученных комбинаций и удаление повторов
 //        echo 'Перебор комбинаций в процессе' . "<br/>";
-        $result1 = array_unique($combinations2);
+        $uniqueArr = array_unique($combinations2);
 
-        $result2 = setOrder($result1);
-        $result2 = super_unique($result2);
-        $result2=array_filter($result2);
+        $SortArr = setOrder($uniqueArr);
+        $SortUniqueArr = super_unique($SortArr);
+        $filtSortUniqueArr=array_filter($SortUniqueArr);
 
 //        echo 'Начало нового перебора' . "<br/>";
-//        print_r($result2);
+//        print_r($filtSortUniqueArr);
         echo "<br/>";
 
         //задаю ассоциативный массив комбинаций
         $combs = array();
-        foreach ($result2 as $temp) {
+        foreach ($filtSortUniqueArr as $temp) {
             $count = count(multiexplode(array(' '), $temp));
             $combs[] = array('words' => $temp, 'count' => $count, 'reps'=> 1,'weight'=>1);
         }
@@ -180,38 +181,7 @@ class InputController extends Controller
                 $sorted2=array_sort($combs,'weight',SORT_DESC);
 
 
-
-
-
-    echo "<table border>";
-    echo "<thead>";
-    echo "<tr>";
-    echo "<td> Слово или фраза</td>";
-    echo "<td> Количество повторений</td>";
-    echo "</tr>";
-    echo "</thead>";
-    echo "<tbody>";
-    foreach ($sorted2 as $item) {
-        echo "<tr>";
-        echo "<td>";
-                echo $item['words'];
-
-        echo "</td>";
-        echo "<td>";
-                echo $item['reps'];
-
-        echo "</td >";
-
-        echo"</tr >";
-    }
-
-    echo"</tbody>";
-    echo"</table>";
-
-
-
-
-        return view('process' );
+        return view('process',compact('sorted2') );
     }
 
 }
