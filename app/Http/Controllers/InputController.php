94ca1d902f32090157f21356658aa1c69a230bd2
rhaegys <rhaegys@gmail.com>
2017-08-14 02:26:14 +0700

    Implemented initial ElasticSearch environment, now onto the actual algorythm

--- app/Http/Controllers/InputController.php
+++ app/Http/Controllers/InputController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use phpDocumentor\Reflection\Types\Nullable;
+
 require 'C:\Users\MagmaDiver\app\vendor/autoload.php'; //надо PATH поковырять
 use Elasticsearch\ClientBuilder;
 
 ini_set('max_execution_time', 36000);
+
 
 class InputController extends Controller
 {
@@ -48,8 +50,9 @@ class InputController extends Controller
 
         return $result;
     }
+
     // функция для сортировки массива по ключу
-    function array_sort($array, $on, $order=SORT_ASC)
+    function array_sort($array, $on, $order = SORT_ASC)
     {
         $new_array = array();
         $sortable_array = array();
@@ -91,97 +94,181 @@ class InputController extends Controller
         return view('welcome');
     }
 
+
     public function process()
     {
+
+        $client = ClientBuilder::create()->build();
+
+        if ($client->indices()->exists(['index' => 'stork'])) {
+            $client->indices()->delete(['index' => 'stork']);
+        }
+
+        $params = [
+            'index' => 'stork',
+            'body' => [
+                'mappings' => [
+                    'product' => [
+                        'properties' => [
+                            'product_name' => [
+                                'type' => 'text',
+                                'analyzer' => 'my_analyzer',
+                            ],
+                        ],
+                    ]
+                ],
+                "settings" => [
+                    "analysis" => [
+                        "analyzer" => [
+                            "my_analyzer" => [
+                                "tokenizer" => "standard",
+                                "char_filter" => [
+                                    "xEnd_Replace",
+                                    "xRus_Replace",
+                                    "eRus_Replace",
+                                    "dot_Replace",
+                                ],
+                                "filter" => [
+                                    "lowercase"
+                                ],
+                            ],
+                        ],
+                        "char_filter" => [
+                            "xEnd_Replace" => [
+                                "type" => "pattern_replace",
+                                "pattern" => "(\\d+)x(?=\\d)",
+                                "replacement" => "$1 ",
+                                "flags" => 'CASE_INSENSITIVE',
+                            ],
+                            "xRus_Replace" => [
+                                "type" => "pattern_replace",
+                                "pattern" => "(\\d+)х(?=\\d)",
+                                "replacement" => "$1 ",
+                                "flags" => 'CASE_INSENSITIVE|UNICODE_CASE',
+                            ],
+                            "eRus_Replace" => [
+                                "type" => "pattern_replace",
+                                "pattern" => "ё",
+                                "replacement" => "е",
+                                "flags" => 'CASE_INSENSITIVE|UNICODE_CASE',
+                            ],
+                            "dot_Replace" => [
+                                "type" => "pattern_replace",
+                                "pattern" => "(\\d+),(?=\\d)",
+                                "replacement" => "$1.",
+                                "flags" => 'CASE_INSENSITIVE|UNICODE_CASE',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        $result = $client->indices()->create($params);
+        if ($result['acknowledged'] != true) {
+            //TODO что-то не то
+            dd($result);
+        }
+        echo "</br>";
+        print_r($result);
+        echo "</br>";
         //ввожу данные
         $input = $_POST["input"];
         $inputstr = explode("\n", $input);
         $inputstr = array_filter($inputstr);
+        $tt=0;
         foreach ($inputstr as $inputline) {
+            $params = [
+                'index' => 'stork',
+                'type' => 'product',
+                'id'=>$tt,
+                'body' => ['product_name' => $inputline]
+            ];
 
-            $items = multiexplode(array(' '), rtrim($inputline));
+            $result1 = $client->index($params);
+            print_r($result1);
+            echo "</br>";
+            $params = [
+                'index' => 'stork',
+                'type' => 'product',
+                'id'=>$tt
+            ];
 
-            //считаю возможные комбинации слов
-            $num_words = count($items);
-            for ($i = 0; $i < $num_words; $i++) {
-                for ($j = $i; $j < $num_words; $j++) {
-                    $combinations = array();
-                    for ($k = $i; $k <= $j; $k++) {
-                        array_push($combinations, $items[$k]);
-                    }
-                    $combinations2[] = implode(' ', $combinations);
-                }
-            }
+            $response = $client->get($params);
+            print_r($response);
+            $tt++;
+            echo "</br>";
 
         }
 
 
-        //вывод полученных комбинаций и удаление повторов
-//        echo 'Перебор комбинаций в процессе' . "<br/>";
-        $uniqueArr = array_unique($combinations2);
-
-        $SortArr = setOrder($uniqueArr);
-        $SortUniqueArr = super_unique($SortArr);
-        $filtSortUniqueArr=array_filter($SortUniqueArr);
-
-//        echo 'Начало нового перебора' . "<br/>";
-//        print_r($filtSortUniqueArr);
-        echo "<br/>";
-
-        //задаю ассоциативный массив комбинаций
-        $combs = array();
-        foreach ($filtSortUniqueArr as $temp) {
-            $count = count(multiexplode(array(' '), $temp));
-            $combs[] = array('words' => $temp, 'count' => $count, 'reps'=> 1,'weight'=>1);
-        }
-
-//        Вывод введенного текста
-        echo 'Введенный текст' . "<br/>";
-        foreach ($inputstr as $inputline) {
-            echo $inputline . "<br/>";
-        }
-        echo "<br/>";
-
-        //счетчик совпадений - не сильно нужен
-        $simcount = 0;
-
-        echo "<br/>";
-
-//   работает
-        //подсчет повторений для каждой комбинации
-        foreach ($combs as &$comblines) {
-            $findme = $comblines['words'];
-            $comblines['reps']=0;
-            foreach ($inputstr as $inputline) {
-                $pos = strpos($inputline, $findme);
-                if ($pos === false) {
-//                    echo $findme . ' не равно ' . $inputline . "<br/>";
-                } else {
-//                    echo $findme . ' равно ' . $inputline . "<br/>";
-                    $simcount++;
-                    ++$comblines['reps'];
-//                    Тут формула для веса
-                    $comblines['weight']=$comblines['reps']*$comblines['count'];
-                    }
-            }
-        }
-//        Выдача информации по повторам
-//        foreach ($combs as $comblines)
-//        {
-//            echo $comblines['words'].' повторяется '.$comblines['reps'].' раз и имеет вес = '.$comblines['weight']."<br/>";
+//
+//        //вывод полученных комбинаций и удаление повторов
+////        echo 'Перебор комбинаций в процессе' . "<br/>";
+//        $uniqueArr = array_unique($combinations2);
+//
+//        $SortArr = setOrder($uniqueArr);
+//        $SortUniqueArr = super_unique($SortArr);
+//        $filtSortUniqueArr=array_filter($SortUniqueArr);
+//
+////        echo 'Начало нового перебора' . "<br/>";
+////        print_r($filtSortUniqueArr);
+//        echo "<br/>";
+//
+//        //задаю ассоциативный массив комбинаций
+//        $combs = array();
+//        foreach ($filtSortUniqueArr as $temp) {
+//            $count = count(multiexplode(array(' '), $temp));
+//            $combs[] = array('words' => $temp, 'count' => $count, 'reps'=> 1,'weight'=>1);
 //        }
 //
+////        Вывод введенного текста
+//        echo 'Введенный текст' . "<br/>";
+//        foreach ($inputstr as $inputline) {
+//            echo $inputline . "<br/>";
+//        }
 //        echo "<br/>";
-//        echo $simcount.' совпадений';
+//
+//        //счетчик совпадений - не сильно нужен
+//        $simcount = 0;
+//
+//        echo "<br/>";
+//
+////   работает
+//        //подсчет повторений для каждой комбинации
+//        foreach ($combs as &$comblines) {
+//            $findme = $comblines['words'];
+//            $comblines['reps']=0;
+//            foreach ($inputstr as $inputline) {
+//                $pos = strpos($inputline, $findme);
+//                if ($pos === false) {
+////                    echo $findme . ' не равно ' . $inputline . "<br/>";
+//                } else {
+////                    echo $findme . ' равно ' . $inputline . "<br/>";
+//                    $simcount++;
+//                    ++$comblines['reps'];
+////                    Тут формула для веса
+//                    $comblines['weight']=$comblines['reps']*$comblines['count'];
+//                    }
+//            }
+//        }
+////        Выдача информации по повторам
+////        foreach ($combs as $comblines)
+////        {
+////            echo $comblines['words'].' повторяется '.$comblines['reps'].' раз и имеет вес = '.$comblines['weight']."<br/>";
+////        }
+////
+////        echo "<br/>";
+////        echo $simcount.' совпадений';
+//
+//
+//
+////        Сортировка массива для вывода на экран
+//
+//                $sorted2=array_sort($combs,'weight',SORT_DESC);
 
 
-
-//        Сортировка массива для вывода на экран
-
-                $sorted2=array_sort($combs,'weight',SORT_DESC);
-
-
-        return view('process',compact('sorted2') );
+        return view('process');
     }
-
 }
+    
--- resources/views/process.blade.php
+++ resources/views/process.blade.php
@@ -6,31 +6,7 @@
     </title>
 </head>
 <body>
-<table border>
-    <thead>
-    <tr>
-        <td> Слово или фраза</td>
-        <td> Количество повторений</td>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach ($sorted2 as $item)
-        <tr>
-            <td>
-                {{$item['words']}}
 
-            </td>
-            <td>
-                {{$item['reps']}}
-
-            </td>
-
-        </tr>
-
-    @endforeach
-
-    </tbody>
-</table>
 
 </body>
 </html>
